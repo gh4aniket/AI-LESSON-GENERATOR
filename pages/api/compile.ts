c07681ed5620acfc7fec25b74345d2d84e1da505
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2025-11-16 16:31:06 +0530

    Convert error message to string in API response

--- pages/api/compile.ts
+++ pages/api/compile.ts
@@ -42,7 +42,7 @@ export default async function handler(req: NextApiRequest,
   } catch (err) {
     return res.status(400).json({
       ok: false,
-      error: err.message,
+      error: String(err.message),
     });
   }
 }
