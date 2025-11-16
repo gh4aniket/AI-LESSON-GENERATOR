c71209a3effa48cd8cb253d50e8a235f6a3b51fc
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2025-11-16 17:12:26 +0530

    Update compile.ts

--- app/api/pages/compile.ts
+++ app/api/pages/compile.ts
@@ -1,1 +1,45 @@
+// pages/api/compile.ts
+import * as esbuild from "esbuild";
+import vm from "vm";
+import type { NextApiRequest, NextApiResponse } from "next";
 
+export default async function handler(tsx:string) {
+  try {
+    const result = await esbuild.build({
+      stdin: {
+        contents: tsx,
+        loader: "tsx",
+        sourcefile: "lesson.tsx",
+      },
+      write: false,
+      bundle: true,
+      platform: "browser",
+      format: "cjs",
+      target: "es2020",
+      external: ["react", "react-dom"],
+    });
+
+    const js = result.outputFiles[0].text;
+
+    // sandbox test
+    const sandbox: any = {
+      module: { exports: {} },
+      exports: {},
+      require: () => ({}),
+    };
+
+    const context = vm.createContext(sandbox);
+    const script = new vm.Script(js);
+    script.runInContext(context);
+
+    return res.status(200).json({
+      ok: true,
+      compiled: js,
+    });
+  } catch (err: any) {
+    return res.status(400).json({
+      ok: false,
+      error: (err.message),
+    });
+  }
+}
