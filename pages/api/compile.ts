a9fc7f8fc0673ece55310b7477ef05a7c29af1a2
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2025-11-16 16:35:01 +0530

    Update compile.ts

--- pages/api/compile.ts
+++ pages/api/compile.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest,
       ok: true,
       compiled: js,
     });
-  } catch (err: Error) {
+  } catch (err: any) {
     return res.status(400).json({
       ok: false,
       error: (err.message),
